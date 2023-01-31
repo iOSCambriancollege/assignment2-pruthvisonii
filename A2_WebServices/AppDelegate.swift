5a7f7f138339381000715ca7934a3e0a9ddca61a
pruthvisonii <106866817+pruthvisonii@users.noreply.github.com>
2023-01-31 17:34:41 -0500

    Update AppDelegate.swift

--- A2_WebServices/AppDelegate.swift
+++ A2_WebServices/AppDelegate.swift
@@ -5,6 +5,7 @@
 //  Created by Cambrian on 2023-01-25.
 //
 
+
 import UIKit
 
 @main
@@ -18,7 +19,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     // MARK: UISceneSession Lifecycle
-
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         // Called when a new scene session is being created.
         // Use this method to select a configuration to create the new scene with.
