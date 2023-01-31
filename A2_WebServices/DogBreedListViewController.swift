cfd05fd442ab3d1661270f918bf2a28370401c54
pruthvisonii <106866817+pruthvisonii@users.noreply.github.com>
2023-01-31 17:47:51 -0500

    Update DogBreedListViewController.swift

--- A2_WebServices/DogBreedListViewController.swift
+++ A2_WebServices/DogBreedListViewController.swift
@@ -1,56 +1,74 @@
 import UIKit
 import SDWebImage
 
-class DogBreedListViewController: UITableViewController {
-
+class DogBreedsTableViewController: UITableViewController {
+    
+    // Dictionary to store breeds and its subbreeds
     var breeds: [String: [String]] = [:]
+    // Array to store keys of the dictionary, i.e., breeds
     var breedKeys = [String]()
-    var breedImageURLs = [String]()
+    // Array to store images of all the breeds
+    var breedImages = [String]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // Clearing selection between presentations
+        self.clearsSelectionOnViewWillAppear = false
+        
+        // Adding activity indicator
         let activityIndicator = UIActivityIndicatorView()
-        activityIndicator.frame = CGRect(x: tableView.center.x, y: tableView.center.y - 100, width: 20, height: 20)
+        activityIndicator.frame = CGRect(x: tableView.center.x, y: tableView.center.y-100, width: 20, height: 20)
         tableView.addSubview(activityIndicator)
         activityIndicator.startAnimating()
         
+        // Fetch breeds and its images in a background thread
         Task {
             do {
+                // Fetching all the breeds
                 self.breeds = try await DogAPI_Helper.fetchBreeds()
+                // Sorting the breed keys
                 self.breedKeys = self.breeds.keys.sorted(by: <)
                 
+                // Fetching images of all the breeds
                 for _ in 0...self.breeds.count - 1 {
-                    let imgURL = try await DogApi_ImageHelper.fetchBreeds()
-                    self.breedImageURLs.append(imgURL)
+                    let image = try await DogApi_ImageHelper.fetchBreeds()
+                    self.breedImages.append(image)
                 }
                 
+                // Reload the table view on the main thread
                 DispatchQueue.main.async {
                     self.tableView.reloadData()
                     activityIndicator.stopAnimating()
                 }
             } catch let error {
-                print("Something went wrong: \(error)")
+                print("something went wrong: \(error)")
             }
         }
     }
-
+    
+    // MARK: - Table View Data Source
+    
     override func numberOfSections(in tableView: UITableView) -> Int {
         return breeds.count
     }
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         let breed = breedKeys[section]
-        return breeds[breed]?.count ?? 1
+        var numberOfSubbreeds = 1
+        if breeds[breed]!.count > 0 {
+            numberOfSubbreeds = breeds[breed]!.count + 1
+        }
+        return numberOfSubbreeds
     }
-
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "breed", for: indexPath) as! SubbreedTableViewCell
-
+        
         let breed = breedKeys[indexPath.section]
-        if let subbreeds = breeds[breed], subbreeds.count > 0, indexPath.row != 0 {
+        if breeds[breed]!.count > 0 && indexPath.row != 0 {
             cell.breedName.text = "\(breed):"
-            cell.subBreedName.text = subbreeds[indexPath.row - 1]
+            cell.subBreedName.text = breeds[breed]![indexPath.row - 1]
         } else {
             cell.breedName.text = breed
             cell.subBreedName.alpha = 0
@@ -67,3 +85,5 @@ class DogBreedListViewController: UITableViewController {
         navigationController!.pushViewController(vc, animated: true)
     }
 }
+
+
