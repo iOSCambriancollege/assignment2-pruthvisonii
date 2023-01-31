c39e352ee931c70be923589eab81fbba61e2c006
pruthvisonii <106866817+pruthvisonii@users.noreply.github.com>
2023-01-30 23:46:09 -0500

    Update ViewController.swift

--- A2_WebServices/ViewController.swift
+++ A2_WebServices/ViewController.swift
@@ -6,14 +6,34 @@
 //
 
 import UIKit
+import SDWebImage
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var breedNameLabel: UILabel!
+    @IBOutlet weak var dogImage: UIImageView!
+    
+    var breed = String()
+    var breeds: [String] = []
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+       // self.breedNameLabel.text = breed
+        
+        Task{
+            do{
+                self.breeds = try await DogApi_ImageFull.fetchBreeds(breed: breed)
+                breedNameLabel.text = "Total Images for \(breed) = " + String(breeds.count) //name and image count
+                self.dogImage.sd_setImage(with: URL(string: self.breeds[0]), placeholderImage: UIImage()) //bringing in the dog image as soon the view is displayed + caching with sdwebimage, 
+            }
+            catch let err{
+                print("something went wrong: \(err)")
+            }
+        }
     }
-
-
+       
+    @IBAction func randomImage(_ sender: Any) { //bringing in new image
+        self.dogImage.sd_setImage(with: URL(string: self.breeds[Int.random(in: 0...self.breeds.count-1 )]), placeholderImage: UIImage())
+    }
 }
-
